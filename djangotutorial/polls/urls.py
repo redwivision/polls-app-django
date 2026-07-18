0cc4b37cc5308682079eb841c9564bda84977bcd
redwivision <lewikb13@gmail.com>
2026-07-18 11:29:13 +0300

    tutorial part1 and some of part2

---
+++ djangotutorial/polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("bye", views.index2, name="index2"),
+]
---
+++ djangotutorial/polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse(200)
+
+def index2(request):
+    return HttpResponse("hi")
